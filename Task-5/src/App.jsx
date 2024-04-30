5c3d6a87be95df110fcfce32b9cffe8e1ea59741
adwaitya10 <adwaitya.118038@gmail.com>
2024-04-30 17:26:44 +0530

    completed task 5

---
+++ Task-5/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import UserRegistrationForm from './components/UserRegistrationForm'
+
+function App() {
+
+
+  return (
+    <>
+      <UserRegistrationForm/>
+    </>
+  )
+}
+
+export default App
